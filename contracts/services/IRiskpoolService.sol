6e4c5fda5a460b396db68a4059747c3277944c6c
matthias zimmermann <zimmermann@xbrain.ch>
2022-07-07 12:55:32 +0200

    add return value to expirePolicy in IRiskpoolService

--- contracts/services/IRiskpoolService.sol
+++ contracts/services/IRiskpoolService.sol
@@ -5,8 +5,8 @@ interface IRiskpoolService {
 
     function createBundle(bytes calldata filter_, uint256 amount_) external returns(uint256 bundleId);
 
-    function collateralizePolicy(uint256 bundleId, bytes32 processId, uint256 amount) external;
-    function expirePolicy(uint256 bundleId, bytes32 processId) external;
+    function collateralizePolicy(uint256 bundleId, bytes32 processId, uint256 collateralAmount) external;
+    function expirePolicy(uint256 bundleId, bytes32 processId) external returns(uint256 collateralAmount);
 
 }
 
