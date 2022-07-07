8bd76eeff4d09dd2b0b6d9626601a09083983241
matthias zimmermann <zimmermann@xbrain.ch>
2022-07-07 13:19:28 +0200

    amend previous commit

--- contracts/modules/IBundle.sol
+++ contracts/modules/IBundle.sol
@@ -42,6 +42,6 @@ interface IBundle {
     function unlock(uint256 bundleId) external;
     function close(uint256 bundleId) external;
 
-    function collateralizePolicy(uint256 bundleId, bytes32 processId, uint256 amount) external;
-    function expirePolicy(uint256 bundleId, bytes32 processId) external;
+    function collateralizePolicy(uint256 bundleId, bytes32 processId, uint256 collateralAmount) external;
+    function expirePolicy(uint256 bundleId, bytes32 processId) external returns(uint256 collateralAmount);
 }
