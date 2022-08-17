255f4147ffaf564ed382bef00f4869d6b76b503f
Marc Doerflinger <marc@doerf.li>
2022-08-17 07:17:20 +0000

    add duplicate error check workflow

---
+++ .github/workflows/scripts/find_duplicate_errorcodes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DUPES=`egrep -or "\"(ERROR\:[A-Z0-9_-]+\:[A-Z0-9_-]+)\"" contracts/* | sort | uniq -cd`
+
+if [ -z "$DUPES" ]; then
+    echo "No duplicate error codes found"
+else
+    echo "Duplicate error codes found:"
+    echo "$DUPES"
+    exit 1
+fi
+
